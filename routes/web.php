3f58c44128f02da46d48cf302d7dab9645b382e2
M511-18\admin <a9101021249@gmail.com>
2017-09-28 11:32:37 +0800

    路由  視圖

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//顯示我們所有任務的清單
 Route::get('/', function () {
     return view('welcome');
+    return view('tasks');
+});
+
+
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    //
+});
+
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
